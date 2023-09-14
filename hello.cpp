2f8f63b607f7ae8cff217183cc2b9a3ac8197996
Noemie Bouche <bouche29u@etu.univ-lorraine.fr>
2023-09-14 10:19:29 +0200

    projet final cmake

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include <iostream>
+#include <HelloProjConfig.h>
+
+
+void hello_world() {
+ std::cout << "Hello World!\n";
+}
+
+void print_version(){
+ std::cout<< "project version is "
+ << HelloProj_VERSION_MAJOR 
+ <<"."
+ << HelloProj_VERSION_MINOR
+ << std::endl;
+} 
+
 int main()
 {
-printf("hello, World !");
-return 0;
+ hello_world();
+ print_version();
 }
